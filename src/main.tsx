9a0d0e329eb99f7887d2194e9b155b251c8726ed
Filipuci <rafaellifipuci@outlook.com>
2026-04-04 12:10:21 -0300

    fix: correct providers import path case

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Providers } from './utils/providers.tsx'
+import { Providers } from './utils/Providers.tsx'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')!).render(
